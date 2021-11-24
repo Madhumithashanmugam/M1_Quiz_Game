4a5a8ea09503ac5993e7a2aa902fe944328d2dcb
Madhushanmugam <94168215+Madhumithashanmugam@users.noreply.github.com>
2021-11-24 17:00:35 +0530

    Add files via upload

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,14 @@
+void main()
+{
+    printf("\n\t\t________________________________________");
+
+     printf("\n\t\t\t        WELCOME ");
+     printf("\n\t\t\t          TO ");
+     printf("\n\t\t\t       QUIZ GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t    TEST YOUR C KNOWLEDGE..SET YOUR HIGHSCORES!!    ") ;
+    printf("Register your name:");
+    gets(name);
+    menu();
+}
