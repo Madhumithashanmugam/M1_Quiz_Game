a17c9d5a020f92261c60f7ad9d6b2e8bb634479f
Madhushanmugam <94168215+Madhumithashanmugam@users.noreply.github.com>
2021-11-24 15:37:57 +0530

    Add files via upload

---
+++ 3_Implementation/inc/quiz.h
@@ -0,0 +1,29 @@
+#ifndef _QUIZ_H_
+#define _QUIZ_H_
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void calculateScore()
+  /**
+ * @brief function to view scores and results 
+ * @return values
+ */
+
+void question()
+
+ /**
+ * @brief function to view questions  
+ * @return values
+ */
+
+void menu()
+
+ /**
+ * @brief function to view rules
+ */
