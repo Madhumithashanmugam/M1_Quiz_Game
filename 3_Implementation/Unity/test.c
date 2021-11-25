5a40bd76b1b8cfdd17eace8db25c0146b3e1a8b5
Madhushanmugam <94168215+Madhumithashanmugam@users.noreply.github.com>
2021-11-25 10:23:33 +0530

    Create test.c

---
+++ 3_Implementation/Unity/test.c
@@ -0,0 +1,36 @@
+#include "unity.h"
+#include "game_quiz.h"
+
+/* Modify these two lines according to the project */
+
+
+
+/* Prototypes for all the test functions */
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void question()
+{
+}
+void menu()
+{
+}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_check_draw);
+  
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
